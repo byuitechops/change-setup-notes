732ffc03d13ef85b986550452104f0a282d2e2d1
YuriiVasiuk <yuravasiuk@gmail.com>
2018-02-06 16:29:08 -0700

    The module is done

--- main.js
+++ main.js
@@ -1,31 +1,97 @@
 /*eslint-env node, es6*/
+/*eslint no-unused-vars:1*/
+/*eslint no-console:0, semi: 2*/
 
-/* Module Description */
+/* The module finds the Setup Notes in modules, renames it, and deletes it from modules.
+   The renamed vertion stays in Pages. */
 
-/* Put dependencies here */
-
-/* Include this line only if you are going to use Canvas API */
-// const canvas = require('canvas-wrapper');
-
-/* View available course object functions */
-// https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
+const canvas = require('canvas-wrapper'),
+    asyncLib = require('async');
 
 module.exports = (course, stepCallback) => {
+    var cID = course.info.canvasOU;
+    var mID, iID;
 
-    /* Used to log successful actions (specific items) */
-    course.log('Category', {'header': 27});
+    // #1 -- get Instructor Resources module 
+    function getModule(getModuleCB) {
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/modules`, function (err, modules) {
+            if (err) {
+                course.error(new Error('error while getting the modules'));
+                getModuleCB(err);
+                return;
+            }
+            var my_modules = '';
+            // this will return the array of objects, but we only expect just one module in it -->
+            my_modules = modules.filter(module => module.name === 'Instructor Resources');
+            if (my_modules === '') {
+                course.message('No Instructor Resources found in the course');
+                stepCallback(null, course);
+            } else {
+                course.message('Retrieved the Instructor Resorces module');
+                // --> so I just return the first element which is my module
+                getModuleCB(null, my_modules[0]);
+            }
+        });
+    }
 
-    /* How to log a generic message. Use in place of console.log */
-    course.message('message');
+    // #2 -- get item information  
+    function getItem(module, getItemCB) {
+        // this just gets the module ID, the item ID, and the item title
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/modules/${module.id}/items`, function (err, moduleItems) {
+            if (err) {
+                course.error(new Error('error while getting the items'));
+                getItemCB(err);
+                return;
+            }
+            moduleItems.forEach(function (item) {
+                if (item.title === 'Setup Notes for Development Team') {
+                    mID = item.module_id;
+                    iID = item.id;
+                }
+            });
+            course.message('Got the item information');
+            getItemCB(null, module);
+        });
+    }
 
-    /* How to report a warning */
-    // course.warning('warning message...');
-    
-    /* How to report an error */
-    course.error(new Error('this is my fake err message'));
+    // #3 -- update the item title
+    function updateItem(module, updateItemCB) {
+        var urlOut = `/api/v1/courses/${cID}/modules/${mID}/items/${iID}`;
+        var new_title = '-Setup Notes & Course Settings';
+        canvas.put(urlOut, {
+            'module_item[title]': new_title
+        }, function (err) {
+            if (err) {
+                course.error(new Error('error while updating the item title'));
+                updateItemCB(err);
+                return;
+            }
+            course.message('Updated the item and the page names');
+            updateItemCB(null, module);
+        });
+    }
 
-    /* You should never call the stepCallback with an error. We want the
-    whole program to run when testing so we can catch all existing errors */
+    // #4 -- deleter the item from the modules
+    function deleteItem(module, deleteItemCB) {
+        var itemToDelete = `/api/v1/courses/${cID}/modules/${mID}/items/${iID}`;
+        canvas.delete(itemToDelete, function (err) {
+            if (err) {
+                course.error(new Error('error while updating the item title'));
+                deleteItemCB(err);
+                return;
+            }
+            course.message('The item has been deleted from the modules');
+            deleteItemCB(null, module);
+        });
+    }
 
-    stepCallback(null, course);
+    asyncLib.waterfall([
+        getModule,
+        getItem,
+        updateItem,
+        deleteItem
+    ],
+        function () {
+            stepCallback(null, course);
+        });
 };
